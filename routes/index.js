67612045074564077226cc68fc067df5afa6c007
LeoFdz <lfernandez79@gmail.com>
2020-10-16 15:35:07 -0500

    created route to write on DB in index.js line 31 onwards

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const { route } = require('./users');
 var router = express.Router();
 
 /* GET home page. */
@@ -20,6 +21,41 @@ router.get("/userlist", function (req, res) {
       "userlist": docs
     });
   });
+});
+
+// Get New User page
+router.get("/newuser", function (req, res) {
+  res.render("newuser", { title: "Add New User" });
+});
+
+// POST to add a user
+router.post("/adduser", function (req, res) {
+
+  // Set our internal DB variable
+  var db = req.db;
+
+  // Get our form values. These rely on the "name" attributes
+  var userName = req.body.username;
+  var userEmail = req.body.useremail;
+
+  // Set collection
+  var collection = db.get("usercollection");
+
+  // Submit to ther DB
+  collection.insert({
+    "username": userName,
+    "email": userEmail
+  },
+    function (err, doc) {
+      if (err) {
+        // if it fails, return error
+        res.send("There is a problem adding the information to the DB");
+      }
+      else {
+        // Forward to success page
+        res.redirect("userlist")
+      }
+    });
 });
 
 module.exports = router;
